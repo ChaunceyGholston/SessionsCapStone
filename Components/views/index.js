da33eec7f9ce1ca25763889465faee0d1b41efcb
Chauncey Gholston <cgholston89@gmail.com>
2021-10-19 15:11:58 -0500

    NEW UPDATE

--- Components/views/index.js
+++ Components/views/index.js
@@ -1,4 +1,4 @@
 export { default as Home } from "./Home";
 export { default as About } from "./About";
-export { default as ContactUs } from "./ContactUs";
+export { default as Contactus } from "./Contactus";
 export { default as Artist } from "./Artist";
