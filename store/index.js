6cb22fbe6fb66b11262f7443f85b7e57ac337a44
Chauncey Gholston <cgholston89@gmail.com>
2021-10-26 18:36:30 -0500

    yep

--- store/index.js
+++ store/index.js
@@ -1,5 +1,5 @@
 export { default as About } from "./About";
 export { default as Artist } from "./Artist";
 export { default as Home } from "./Home";
-export { default as ContactUs } from "./ContactUs";
+export { default as Contactus } from "./Contactus";
 export { default as Links } from "./Links";
