142836f081b81caa5c4a447126cb119a93d65cbe
Chauncey Gholston <cgholston89@gmail.com>
2021-11-10 21:22:24 -0600

    wednesday 9:22pm

--- components/views/index.js
+++ components/views/index.js
@@ -4,3 +4,4 @@ export { default as Contactus } from "./Contactus";
 export { default as Artist } from "./Artist";
 export { default as Kythiaartist } from "./Kythiaartist";
 export { default as Shawartist } from "./Shawartist";
+export { default as Dawnartist } from "./Dawnartist";
